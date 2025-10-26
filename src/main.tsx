9d68227c9a0edb76bc40c7269dc5668022c6dca7
MissyCanada2019 <terri.bertin57@gmail.com>
2025-10-26 07:30:55 -0400

    Wrap App in HelmetProvider and StrictMode

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,15 @@
+import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
 import { PerformanceMonitor } from "./components/PerformanceMonitor";
+import { HelmetProvider } from "react-helmet-async";
 
 createRoot(document.getElementById("root")!).render(
-  <>
-    <PerformanceMonitor />
-    <App />
-  </>
+  <React.StrictMode>
+    <HelmetProvider>
+      <PerformanceMonitor />
+      <App />
+    </HelmetProvider>
+  </React.StrictMode>
 );
